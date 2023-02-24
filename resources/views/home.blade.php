3d349b9881b2f478b854faeb52404ab4fcb238f5
daniel6etec <daniel.rufino6@etec.sp.gov.br>
2023-02-24 17:49:13 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+<a href="{{ url('/empresas') }}">empresas</a><br>
+<a href="{{ url('/contatos') }}">contatos</a><br>
+<a href="{{ url('/produtos') }}">produtos</a>
+</div>
+</body>
+</html>
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Produtos</title>
+</head>
+<body>
+<a href="{{ url('') }}">home</a><br>
+<a href="{{ url('/contatos') }}">contatos</a><br>
+<a href="{{ url('/produtos') }}">produtos</a>
+</body>
+</html>
